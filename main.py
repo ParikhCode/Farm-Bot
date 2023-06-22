05374257f1192b886b4b2ec4482d7bb6247fda7e
Parth Parikh <parthpar22@gmail.com>
2023-06-21 22:03:17 -0400

    built AI training model

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ for intent in data["intents"]: #  STEMMING
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -60,4 +60,18 @@ for x, doc in enumerate(docs_x):
 training = np.array(training)
 output = np.array(output)
 
-print("Hello")
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])                   # start off with the input data with length training
+net = tflearn.fully_connected(net, 8)                                      # 2 hidden layers with 8 nerons fully connected
+net = tflearn.fully_connected(net, 8)                                       
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")   # another fully connected hidden layer that has neurons 
+                                                                           # representing each of our classes
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)                                                   # DNN is a type of neural network
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
